50e8eef51b86e4b453ae001564d65df40f17a96c
Chau Vu <chv004@ucsd.edu>
2020-04-29 23:04:18 -0700

    basic hello world

---
+++ popup.js
@@ -0,0 +1,7 @@
+(function() {
+    window.onload = function() {
+        document.getElementById("name").addEventListener("keyup", function(e) {
+            document.getElementById("greet").innerHTML = "Hello " + e.target.value;
+        })
+    };
+})();

96e1b452d3b1af53a07ba05287b11f53d0e182fa
Gary Chew <garylchew@gmail.com>
2020-05-06 13:15:23 -0700

    Add prettier, ESLint, and .gitignore

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-(function() {
-    window.onload = function() {
-        document.getElementById("name").addEventListener("keyup", function(e) {
-            document.getElementById("greet").innerHTML = "Hello " + e.target.value;
-        })
-    };
+(function () {
+  window.onload = function () {
+    document.getElementById("name").addEventListener("keyup", function (e) {
+      document.getElementById("greet").innerHTML = `Hello ${e.target.value}`;
+    });
+  };
 })();

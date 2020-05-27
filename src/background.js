613c62064ef982a7afdd6e264e6d0bb6f8c20748
scheung2 <cheungstephen3@gmail.com>
2020-05-27 15:31:08 -0700

    changed siteblocking in focus mode to block by domain

--- src/background.js
+++ src/background.js
@@ -1,17 +1,25 @@
+const psl = require('psl');
+
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   chrome.storage.sync.get('isFocusModeEnabled', (obj) => {
     // check if Focus Mode is on
     if (obj.isFocusModeEnabled) {
-      chrome.storage.sync.get(
-        'focusedTabGroupUrls',
-        (focusedTabGroupUrlsObj) => {
-          const allowedDomains = focusedTabGroupUrlsObj.focusedTabGroupUrls;
-          // if current tab's url isn't in allowedDomains, block the site
-          if (!allowedDomains.some((domain) => tab.url.includes(domain))) {
-            chrome.tabs.executeScript(tabId, { file: 'siteBlocker.bundle.js' });
+      if (tab.url != null && !tab.url.startsWith('chrome://')) {
+        chrome.storage.sync.get(
+          'focusedTabGroupUrls',
+          (focusedTabGroupUrlsObj) => {
+            const allowedUrls = focusedTabGroupUrlsObj.focusedTabGroupUrls;
+            const tabDomain = psl.parse(tab.url.split('/')[2]).domain;
+            const hasSameDomain = (u) => u.includes(tabDomain);
+            // if current tab's url isn't in allowedDomains, block the site
+            if (!allowedUrls.some(hasSameDomain)) {
+              chrome.tabs.executeScript(tabId, {
+                file: 'siteBlocker.bundle.js',
+              });
+            }
           }
-        }
-      );
+        );
+      }
     }
   });
 });

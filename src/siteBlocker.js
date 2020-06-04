46a505836fff01455b162b1c3945cce6e7d5f657
Chau Vu <chv004@ucsd.edu>
2020-06-04 14:55:41 -0700

    Modified blocking overlay UI to only 2 buttons and randomly generated headings

--- src/siteBlocker.js
+++ src/siteBlocker.js
@@ -1,5 +1,15 @@
 import './styles/BlockPopup.css';
 
+const headingTexts = [
+  'Tsk tsk tsk 😤, you should be focusing on [tabgroup]!',
+  "Shouldn't you be working on [tabgroup]? 🤔",
+  'Quit 🐴-ing around, get back to work on [tabgroup]!',
+  "What do you think you're doing 👀? Focus on [tabgroup]!",
+  "I guess [tabgroup] isn't that important to you 🥺?",
+  'You wanted to focus on [tabgroup] you said 🤥',
+  'Seriously 😟, you need to work on [tabgroup]!',
+];
+
 document.body.style.position = 'relative';
 document.body.style.zIndex = '-1';
 // Overlay div
@@ -11,30 +21,23 @@ const mainDiv = document.createElement('div');
 mainDiv.setAttribute('class', 'main');
 overlay.appendChild(mainDiv);
 
-// FocusMode Text
-const focusModeText = document.createElement('p');
-focusModeText.setAttribute('class', 'focusModeText');
-focusModeText.innerHTML = 'You are in Focus Mode for ';
-mainDiv.append(focusModeText);
-
-// Focus Group Text
-const focusGroupText = document.createElement('p');
-focusGroupText.setAttribute('class', 'focusGroupText');
-chrome.storage.sync.get('focusedTabGroupName', (obj) => {
-  focusGroupText.innerHTML = obj.focusedTabGroupName;
-});
-mainDiv.append(focusGroupText);
-
 // Heading
 const heading = document.createElement('p');
 heading.setAttribute('class', 'heading');
-heading.innerHTML = `${window.location.host} is blocked!`;
+chrome.storage.sync.get('focusedTabGroupName', (obj) => {
+  const focusGroupText = obj.focusedTabGroupName;
+  const randomIndex = Math.floor(Math.random() * headingTexts.length);
+  heading.innerHTML = headingTexts[randomIndex].replace(
+    '[tabgroup]',
+    focusGroupText
+  );
+});
 mainDiv.appendChild(heading);
 
 // Close Button
 const closeBtn = document.createElement('button');
 closeBtn.setAttribute('class', 'button');
-closeBtn.innerHTML = 'Close Tab';
+closeBtn.innerHTML = `You got me, close ${window.location.host}`;
 closeBtn.style.background = '#18b53a';
 closeBtn.onclick = () => {
   chrome.runtime.sendMessage({ action: 'close' }, (response) => {
@@ -43,35 +46,25 @@ closeBtn.onclick = () => {
 };
 mainDiv.appendChild(closeBtn);
 
-// Unblock Button
-const unblockBtn = document.createElement('button');
-unblockBtn.setAttribute('class', 'button');
-unblockBtn.innerHTML = 'Unblock Once';
-unblockBtn.style.background = '#0078f0';
-unblockBtn.onclick = () => {
+// Unblock for Session Button
+const unblockSessionBtn = document.createElement('button');
+unblockSessionBtn.setAttribute('class', 'button');
+unblockSessionBtn.setAttribute('id', 'unblockSessionBtn');
+unblockSessionBtn.innerHTML = `Please, I really need ${window.location.host}`;
+unblockSessionBtn.style.background = '#ff3b3b';
+unblockSessionBtn.onclick = () => {
   chrome.runtime.sendMessage(
-    { action: 'unblock', url: window.location.origin },
+    {
+      action: 'unblockSession',
+      title: document.title,
+      url: window.location.origin,
+    },
     (response) => {
       console.log(response);
     }
   );
 };
-mainDiv.appendChild(unblockBtn);
-
-// Add to Group Button
-const addBtn = document.createElement('button');
-addBtn.setAttribute('class', 'button');
-addBtn.innerHTML = 'Add to Group';
-addBtn.style.background = '#ff3b3b';
-addBtn.onclick = () => {
-  chrome.runtime.sendMessage(
-    { action: 'add', title: document.title, url: window.location.origin },
-    (response) => {
-      console.log(response);
-    }
-  );
-};
-mainDiv.appendChild(addBtn);
+mainDiv.appendChild(unblockSessionBtn);
 
 document.body.style.padding = '0';
 document.body.style.margin = '0';

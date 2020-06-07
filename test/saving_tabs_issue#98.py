8e686bebdfef844ca7c8b1e5185a455dce9dd568
David Dai <daiwei19971009@gmail.com>
2020-06-06 22:36:09 -0700

    fix saving tabs selenium test

--- test/saving_tabs_issue#98.py
+++ test/saving_tabs_issue#98.py
@@ -17,6 +17,7 @@ options.add_argument("load-extension=./project/build/")
 #options.add_extension('./build.crx')
 options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
 options.add_argument("--no-sandbox") # Bypass OS security model
+options.add_argument("--start-maximized")
 
 
 driver = webdriver.Chrome(options=options)

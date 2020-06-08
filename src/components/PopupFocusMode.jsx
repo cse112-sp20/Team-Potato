e2010496ccdeddd095b07d8c2db628a3cce0dc72
cbyeh <43252771+cbyeh@users.noreply.github.com>
2020-06-07 18:26:28 -0700

    Notification on focus mode end, format timer, max time 3 hrs (#154)
    * Try create notifcation at timer end, not working
    
    * Fix trailing 0 for hrs timer, new max time 3 hrs
    
    * Test notifications on Go Back button
    
    * Remove end focus mode callback
    
    * Remove back button testing
    
    Co-authored-by: Chris <cyeh@ucsd.edu>

--- src/components/PopupFocusMode.jsx
+++ src/components/PopupFocusMode.jsx
@@ -122,6 +122,7 @@ class PopupFocusMode extends React.Component {
       chrome.storage.sync.set({ focusedTabGroupUrls: [] });
       this.setState({ isFocusModeEnabled: false });
       chrome.storage.sync.set({ isFocusModeEnabled: false });
+      chrome.runtime.sendMessage({ msg: 'end' });
     };
 
     /**
@@ -134,13 +135,14 @@ class PopupFocusMode extends React.Component {
       chrome.storage.sync.set({
         focusedTabGroupUrls: tabGroupUrls,
       });
-      /** update the state and set chrome storage to start focus mode */
+      /** update the states */
       this.setState({ isFocusModeEnabled: true });
       chrome.storage.sync.set({ shouldDisplayFocusMode: true });
       chrome.storage.sync.set({ isFocusModeEnabled: true });
-      chrome.runtime.sendMessage({ msg: 'start' });
       /** pass in the time for the initial time of the clock */
       chrome.storage.sync.set({ initClockTime: clock });
+      /** set chrome storage to start focus mode */
+      chrome.runtime.sendMessage({ msg: 'start' });
       /** launch the focus mode */
       this.launchFocusMode();
     };
@@ -186,13 +188,6 @@ class PopupFocusMode extends React.Component {
           direction="backward"
           startImmediately={false}
           formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
-          checkpoints={{
-            time: 0,
-            callback: () => {
-              endFocusMode();
-              hideFocusMode();
-            },
-          }}
         >
           {({ start, stop, setTime, getTime }) => (
             <>
@@ -209,7 +204,7 @@ class PopupFocusMode extends React.Component {
                     }
                   }}
                 >
-                  <Timer.Hours />
+                  <Timer.Hours formatValue={(value) => `${value}`} />
                   :
                   <Timer.Minutes />
                   :
@@ -223,7 +218,7 @@ class PopupFocusMode extends React.Component {
                     defaultValue={60}
                     min={5}
                     step={5}
-                    max={120}
+                    max={180}
                     snapDragDisabled={false}
                     renderThumb={(props, state) => (
                       <div {...props}>{[setTime(60000 * state.valueNow)]}</div>

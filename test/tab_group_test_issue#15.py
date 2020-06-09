95536f075b9f387aef68313cfd90e606dbf75652
David Dai <daiwei19971009@gmail.com>
2020-06-08 21:52:04 -0700

    Update tab_group_test_issue#15.py

--- test/tab_group_test_issue#15.py
+++ test/tab_group_test_issue#15.py
@@ -15,9 +15,10 @@ CRED = '\033[91m'
 CGREEN = '\33[32m'
 CYELLOW = '\33[33m'
 CEND = '\033[0m'
-
+​
 print(os.getcwd())
-options = Options()
+options = webdriver.ChromeOptions()
+​
 print("loading packed extension")
 options.add_argument("load-extension=./project/build/")
 # overcome limited resource problems
@@ -29,15 +30,15 @@ options.add_argument("--start-maximized")
 print("set up driver")
 # creating chrome driver
 driver = webdriver.Chrome(options=options)
-
+​
 # assigning the extensions uid
 uid = "flfgpjanhbdjakbkafipakpfjcmochnp"
 # opening the popup.html page
 driver.get("chrome-extension://" + uid + "/popup.html")
-
+​
 '''
 Test #3
-
+​
 Description: This test checks whether the "Open Potato Tab Menu" is rendered properly
 '''
 print(CYELLOW + "Running Test 3" + CEND)
@@ -47,7 +48,7 @@ for i in open_tab_buttons:
     if i.text != "Start":
         open_tab_button = i
 try:
-    assert open_tab_button.text == "Open Potato Tab"
+    assert open_tab_button.text == "Open Flow"
 except AssertionError:
     print(CRED + "Test 3 Failed" + CEND)
     print(CRED + "Open Potato Tab button not rendered" + CEND)
@@ -55,12 +56,12 @@ except AssertionError:
 else:
     print(CGREEN + "Test 3 Passed")
 print("-----------------------")
-
+​
 '''
 Test #4
-
-Description: This test checks whether the "Open Potato Tab Menu" is working properly
-
+​
+Description: This test checks whether the "Open Flow Tab Menu" is working properly
+​
 '''
 print(CYELLOW + "Running Test 4" + CEND)
 # clicking on the open potato tab button
@@ -69,20 +70,20 @@ open_tab_button.click()
 driver.switch_to.window(driver.window_handles[-1])
 post_press_title = driver.title
 try:
-    assert post_press_title == "Potato Tab Menu"
+    assert post_press_title == "Flow Menu"
 except AssertionError:
     print(CRED +"Test 4 Failed" + CEND)
-    print("Potato Tab Menu not opened on button click")
+    print(CRED + "Flow Tab Menu not opened on button click" + CEND)
     all_tests_check = 1
 else:
     print(CGREEN + "Test 4 Passed" + CEND)
 print("-----------------------")
-
+​
 '''
 Test #5
-
+​
 Description: This test checks if adding tab groups works properly
-
+​
 '''
 print(CYELLOW + "Running Test 5" + CEND)
 # finding add group button and clicking it
@@ -113,12 +114,12 @@ except AssertionError:
 else:
     print(CGREEN + "Test 5 Passed" + CEND)
 print("-----------------------")
-
+​
 '''
 Test Number #6
-
+​
 Description: Testing tab group header is rendering correctly
-
+​
 '''
 print(CYELLOW + "Running Test 6" + CEND)
 page_header = driver.find_element_by_class_name("tabGroupsHeader")
@@ -134,7 +135,7 @@ else:
 print("-----------------------")
 assert all_tests_check == 0, CRED + "Some Tests Failed" + CEND
 print(CGREEN + "All Tests Passed" + CEND)
-
+​
 # writing coverage details
 coverage_json_file = open("./project/.nyc_output/#15.json","w+")
 json.dump(driver.execute_script("return window.__coverage__;"), coverage_json_file)

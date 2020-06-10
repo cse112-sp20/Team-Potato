9da53b17c3f2384918fba845967e2bf79785bd56
David Dai <daiwei19971009@gmail.com>
2020-06-10 14:15:55 -0700

    Update active_tabs_test_issue#33.py

--- test/active_tabs_test_issue#33.py
+++ test/active_tabs_test_issue#33.py
@@ -25,24 +25,27 @@ options = webdriver.ChromeOptions()
 
 print("loading packed extension")
 options.add_argument("load-extension=/Users/dwei/Desktop/Team-Potato/build/")
-# overcome limited resource problems
-options.add_argument("--disable-dev-shm-usage")
 # Bypass OS security model
 options.add_argument("--no-sandbox")
+options.add_argument("--disable-setuid-sandbox") 
+
 # starting window maximized to prevent any scrolling issues
 options.add_argument("--start-maximized")
 
-#options.add_argument("--headless")
+# overcome limited resource problems
 options.add_argument("--disable-gpu")
-options.add_argument("--ignore-certificate-errors")
+options.add_argument("--disable-dev-shm-usage")
+
 options.add_argument("--allow-insecure-localhost")
 options.add_argument("--allow-running-insecure-content")
+#options.add_argument("--headless")
 options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--ignore-certificate-errors")
 
 
 print("set up driver")
 # creating chrome driver
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome('test/chromedriver', options=options)
 
 # assigning the extensions uid
 uid = "flfgpjanhbdjakbkafipakpfjcmochnp"

013dbd9383778a031df78eb91bbbce65a8df20c0
David Dai <daiwei19971009@gmail.com>
2020-06-10 12:06:20 -0700

    disable infobars and set in windows chromedriver env

--- test/active_tabs_test_issue#33.py
+++ test/active_tabs_test_issue#33.py
@@ -31,6 +31,9 @@ options.add_argument("--no-sandbox")
 # starting window maximized to prevent any scrolling issues
 options.add_argument("--start-maximized")
 
+options.add_argument("--start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-gpu")
 options.add_argument("--ignore-certificate-errors")
 options.add_argument("--test-type")
 options.add_argument("test-type")

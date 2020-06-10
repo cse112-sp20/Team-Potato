6da95ded70c2a0ab0a56d37253ac4812d6c55b15
David Dai <daiwei19971009@gmail.com>
2020-06-10 11:10:13 -0700

    add headless to test circleci

--- test/active_tabs_test_issue#33.py
+++ test/active_tabs_test_issue#33.py
@@ -31,6 +31,7 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 # starting window maximized to prevent any scrolling issues
 options.add_argument("--start-maximized")
+options.add_arguments("--headless")
 print("set up driver")
 # creating chrome driver
 driver = webdriver.Chrome(options=options)
